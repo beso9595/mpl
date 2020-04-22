7a3718e43fd368e83576a513c25de95ba065c2ad
Beso Bantsadze <beso.bantsadze@gmail.com>
2020-04-23 03:56:25 +0400

    made search

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', {title: 'Express'});
 });
+
+router.get('/list/:user/:dir', (req, res, next) => {
+  fs.readdir(`/home/${req.params.user}/${req.params.dir}`, (err, files) => {
+    //handling error
+    if (err) {
+      res.status(500);
+      res.json();
+    } else {
+      res.json(files.filter(file => {
+        const parts = file.split('.');
+        return parts.length > 1 && parts[parts.length - 1] === 'mp3';
+      }));
+    }
+  });
+});
+
+router.get('/file/:user/:dir/:title', (req, res, next) => {
+  const path = `/home/${req.params.user}/${req.params.dir}/${req.params.title}`;
+  res.download(path);
+});
+
+
 
 module.exports = router;
