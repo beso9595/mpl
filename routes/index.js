7f25dbb566b2dab799e1f0670b965abd3a021051
Beso Bantsadze <beso.bantsadze@gmail.com>
2020-04-24 00:00:01 +0400

    remove user

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,27 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/', (req, res, next) => {
-  res.render('index', {title: 'Express'});
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/list/:user/:dir', (req, res, next) => {
-  fs.readdir(`/home/${req.params.user}/${req.params.dir}`, (err, files) => {
-    //handling error
-    if (err) {
-      res.status(500);
-      res.json();
-    } else {
-      res.json(files.filter(file => {
-        const parts = file.split('.');
-        return parts.length > 1 && parts[parts.length - 1] === 'mp3';
-      }));
-    }
-  });
+router.get('/list/:dir', (req, res, next) => {
+    fs.readdir(`/home/${process.env.USER}/${req.params.dir}`, (err, files) => {
+        if (err) {
+            res.status(500);
+            res.json();
+        } else {
+            res.json(files.filter(file => {
+                const parts = file.split('.');
+                return parts.length > 1 && parts[parts.length - 1] === 'mp3';
+            }));
+        }
+    });
 });
 
-router.get('/file/:user/:dir/:title', (req, res, next) => {
-  const path = `/home/${req.params.user}/${req.params.dir}/${req.params.title}`;
-  res.download(path);
+router.get('/file/:dir/:title', (req, res, next) => {
+    const path = `/home/${process.env.USER}/${req.params.dir}/${req.params.title}`;
+    res.download(path);
 });
-
 
 
 module.exports = router;
